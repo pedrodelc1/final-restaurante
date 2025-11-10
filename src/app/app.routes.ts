9293a1d627960ef7cc2a6a61841afd707c744bde
pedrodelc1 <pedrodelcarlo@icloud.com>
2025-11-10 19:18:40 -0300

    creacion de componentes y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Articulo } from './pages/articulo/articulo';
+import { Carrito } from './pages/carrito/carrito';
+import { Rubro } from './pages/rubro/rubro';
+import { Buscar } from './pages/buscar/buscar';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home
+    },
+    {
+        path: "Carrito",
+        component: Carrito
+    },
+    {
+        path: "Articulo",
+        component: Articulo
+    },
+    {
+        path: "Rubro",
+        component: Rubro
+    },
+    {
+        path: "Buscar",
+        component: Buscar
+    },
+];
